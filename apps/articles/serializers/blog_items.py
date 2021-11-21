401fe1a4ec9733857c75eb8b2cc3042cda2bb2f0
Nikolay Kiryanov <nikolay@kiryanov.ru>
2021-11-22 00:51:00 +0300

    Blog items updates: - added preambula field - added persons array - added links to other blogs

--- apps/articles/serializers/blog_items.py
+++ apps/articles/serializers/blog_items.py
@@ -1,25 +1,29 @@
 from rest_framework import serializers
 
-from apps.articles.models import BlogItem
+from apps.articles.models import BlogItem, BlogPerson
 from apps.content_pages.serializers import BaseContentPageSerializer
 
 
-class BlogItemSerializer(
-    BaseContentPageSerializer,
-    serializers.ModelSerializer,
-):
+class BlogPersonSerializer(serializers.ModelSerializer):
+    """Merges `Person` and `BlogPerson` fields in one serialized object."""
+
+    id = serializers.SlugRelatedField(
+        source="person",
+        slug_field="id",
+        read_only=True,
+    )
+    full_name = serializers.SlugRelatedField(
+        source="person",
+        slug_field="full_name",
+        read_only=True,
+    )
+
     class Meta:
-        model = BlogItem
+        model = BlogPerson
         fields = (
             "id",
-            "title",
-            "description",
-            "image",
-            "author_url",
-            "author_url_title",
-            "contents",
-            "created",
-            "modified",
+            "full_name",
+            "role",
         )
 
 
@@ -30,5 +34,36 @@ class BlogItemListSerializer(serializers.ModelSerializer):
             "id",
             "title",
             "description",
+            "author_url_title",
             "image",
+        )
+
+
+class BlogItemSerializer(
+    BaseContentPageSerializer,
+    serializers.ModelSerializer,
+):
+    persons = BlogPersonSerializer(
+        source="blog_persons",
+        many=True,
+    )
+    blogs = BlogItemListSerializer(
+        many=True,
+    )
+
+    class Meta:
+        model = BlogItem
+        fields = (
+            "id",
+            "title",
+            "description",
+            "preamble",
+            "image",
+            "author_url",
+            "author_url_title",
+            "contents",
+            "persons",
+            "blogs",
+            "created",
+            "modified",
         )
